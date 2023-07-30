af2d13e5184bbebd552c3a7489ca2d576b2a54d4
Sithara Saman Kumari <sitharakumari97@gmail.com>
2023-07-30 12:30:11 +0000

    secomd commit

--- Sunday01.java
+++ Sunday01.java
@@ -1,5 +1,6 @@
 public class Sunday01{
     public static void main(String[] args) {
         System.out.println("this is my first try with git hub repository");
+        System.out.println("I am going to build the second commit");
     }
 }
